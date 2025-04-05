6ad4718bd065f1d62668a4f15986ea26c1c05e7f
sule-yavuz-dev <sule.yavuz.dev@gmail.com>
2025-04-05 14:05:01 +0300

    game works-project done

--- mole.js
+++ mole.js
@@ -1,6 +1,7 @@
 let currMoleTile;
 let currPlantTile;
-
+let score = 0;
+let gameOver = false;
 
 
 window.onload = function(){
@@ -12,6 +13,7 @@ function setGame(){
        // <div id="0-8"></div>
         let tile = document.createElement("div");
         tile.id=i.toString();
+        tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
     setInterval(setMole, 1000);
@@ -23,6 +25,9 @@ function getRandomTile(){
     return num.toString();
 }
 function setMole(){
+    if(gameOver){
+        return;
+    }
     if (currMoleTile){
         currMoleTile.innerHTML = "";
     }
@@ -39,6 +44,9 @@ function setMole(){
 }
 
 function setPlant(){
+    if(gameOver){
+        return;
+    }
     if(currPlantTile){
         currPlantTile.innerHTML = "";
     }
@@ -52,4 +60,19 @@ function setPlant(){
     }
     currPlantTile = document.getElementById(num);
     currPlantTile.appendChild(plant);
+}
+
+function selectTile(){
+    if(gameOver){
+        return;
+    }
+if(this == currMoleTile){
+    score += 10;
+    document.getElementById("score").innerText = score.toString();
+
+}
+else if(this == currPlantTile){
+    document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+    gameOver = true;
+}
 }
